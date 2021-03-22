c80ff21d0c8ae690353c3a34e3c74158428dc477
rafaelitajahy <rafaelitajahy@gmail.com>
2021-03-22 12:36:56 -0300

    Finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App'
+import { Button } from '../components/Button';
+
+import '../styles/sidebar.scss';
+
+interface SibebarProps {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, handleClickButton}: SibebarProps) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
